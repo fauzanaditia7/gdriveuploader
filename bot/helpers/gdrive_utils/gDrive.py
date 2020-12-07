11cee6e47f7d34c80b518164c649e8dbb0212a16
fauzanaditia7 <64374478+fauzanaditia7@users.noreply.github.com>
2020-12-08 01:16:29 +0700

    Update gDrive.py

--- bot/helpers/gdrive_utils/gDrive.py
+++ bot/helpers/gdrive_utils/gDrive.py
@@ -154,7 +154,7 @@ class GoogleDrive:
       filesize = humanbytes(os.path.getsize(file_path))
       body = {
           "name": filename,
-          "description": "Uploaded using @UploadGdriveBot",
+          "description": "Uploaded by ParaboyMirror",
           "mimeType": mime_type,
       }
       body["parents"] = [self.__parent_id]
